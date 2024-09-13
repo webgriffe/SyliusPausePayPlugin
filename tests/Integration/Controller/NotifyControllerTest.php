be3e81b715ab57133ae168863a8e86c17ed1a21e
Luca Gallinari <luke.gallinari@gmail.com>
2024-09-13 09:49:00 +0200

    Add cancel and not found test cases

--- tests/Integration/Controller/NotifyControllerTest.php
+++ tests/Integration/Controller/NotifyControllerTest.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Fidry\AliceDataFixtures\Loader\PurgerLoader;
 use Payum\Bundle\PayumBundle\Controller\NotifyController;
 use Payum\Core\Model\Identity;
+use Payum\Core\Reply\HttpResponse;
 use Payum\Core\Storage\StorageInterface;
 use Sylius\Bundle\PayumBundle\Model\PaymentSecurityTokenInterface;
 use Sylius\Component\Core\Model\Payment;
@@ -18,6 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Webgriffe\SyliusPausePayPlugin\Client\PaymentState;
 use Webgriffe\SyliusPausePayPlugin\Payum\PausePayApi;
+use Webgriffe\SyliusPausePayPlugin\ValueObject\WebhookPayload;
 
 final class NotifyControllerTest extends KernelTestCase
 {
@@ -51,10 +53,11 @@ final class NotifyControllerTest extends KernelTestCase
     {
         $this->fixtureLoader->load(
             [
-                self::FIXTURE_BASE_DIR . '/test_that_it_confirms_order_payment.yaml',
+                self::FIXTURE_BASE_DIR . '/' . $this->getName() . '.yaml',
                 self::FIXTURE_BASE_DIR . '/customers.yaml',
                 self::FIXTURE_BASE_DIR . '/products.yaml',
                 self::FIXTURE_BASE_DIR . '/payment_methods.yaml',
+                self::FIXTURE_BASE_DIR . '/channels.yaml',
             ],
         );
 
@@ -62,16 +65,7 @@ final class NotifyControllerTest extends KernelTestCase
         $payment = $this->paymentRepository->findAll()[0];
         $this->associateTokenToPayment($payment);
 
-        $requestContent = <<<JSON
-{
-    "orderID":"0b58e4e4-1edc-4f2c-991f-112db59e982d",
-    "eventType": "order.ok",
-    "eventID":"ed8a2511-f60b-4a2b-ac20-02c7a93ccd13",
-    "createdAt":"2024-05-24T16:12:38Z"
-}
-
-JSON;
-
+        $requestContent = $this->getPausePayNotifyPayload();
         $mainRequest = new Request(['gateway' => PausePayApi::GATEWAY_CODE], [], [], [], [], [], $requestContent);
         $this->requestStack->push($mainRequest);
 
@@ -80,7 +74,54 @@ JSON;
         $this->entityManager->refresh($payment);
         self::assertSame(PaymentState::SUCCESS, $payment->getDetails()['status']);
         self::assertSame(PaymentInterface::STATE_COMPLETED, $payment->getState());
+
+        $order = $payment->getOrder();
+        self::assertCount(1, $order->getPayments());
         // todo: check if notify token has been removed?
+    }
+
+    public function test_that_it_cancel_order_payment(): void
+    {
+        $this->fixtureLoader->load(
+            [
+                self::FIXTURE_BASE_DIR . '/' . $this->getName() . '.yaml',
+                self::FIXTURE_BASE_DIR . '/customers.yaml',
+                self::FIXTURE_BASE_DIR . '/products.yaml',
+                self::FIXTURE_BASE_DIR . '/payment_methods.yaml',
+                self::FIXTURE_BASE_DIR . '/channels.yaml',
+            ],
+        );
+
+        /** @var PaymentInterface $payment */
+        $payment = $this->paymentRepository->findAll()[0];
+        $this->associateTokenToPayment($payment);
+
+        $requestContent = $this->getPausePayNotifyPayload(WebhookPayload::EVENT_TYPE_KO);
+        $mainRequest = new Request(['gateway' => PausePayApi::GATEWAY_CODE], [], [], [], [], [], $requestContent);
+        $this->requestStack->push($mainRequest);
+
+        $this->controller->doUnsafeAction($mainRequest);
+
+        $this->entityManager->refresh($payment);
+        self::assertSame(PaymentState::CANCELLED, $payment->getDetails()['status']);
+        self::assertSame(PaymentInterface::STATE_CANCELLED, $payment->getState());
+
+        $order = $payment->getOrder();
+        self::assertCount(2, $order->getPayments());
+        // todo: check if notify token has been removed?
+    }
+
+    public function test_that_it_returns_not_found_rsponse_when_pause_pay_order_is_not_found(): void
+    {
+        $this->fixtureLoader->load([self::FIXTURE_BASE_DIR . '/payment_methods.yaml',],);
+
+        $requestContent = $this->getPausePayNotifyPayload(WebhookPayload::EVENT_TYPE_OK, 'non-existing-order-id');
+        $mainRequest = new Request(['gateway' => PausePayApi::GATEWAY_CODE], [], [], [], [], [], $requestContent);
+        $this->requestStack->push($mainRequest);
+
+        $this->expectExceptionObject(new HttpResponse('Not found', 404));
+
+        $this->controller->doUnsafeAction($mainRequest);
     }
 
     /**
@@ -92,5 +133,20 @@ JSON;
         $token = $this->tokenStorage->find('3ocZLKOx41X04m63TnS9nrBFP6CN6Tu_lHBPvpGwn1o');
         $token->setDetails(new Identity($payment->getId(), Payment::class));
         $this->tokenStorage->update($token);
+    }
+
+    private function getPausePayNotifyPayload(
+        string $eventType = WebhookPayload::EVENT_TYPE_OK,
+        string $orderId = '0b58e4e4-1edc-4f2c-991f-112db59e982d'
+    ): string {
+        return <<<JSON
+{
+    "orderID": "$orderId",
+    "eventType": "$eventType",
+    "eventID": "ed8a2511-f60b-4a2b-ac20-02c7a93ccd13",
+    "createdAt": "2024-05-24T16:12:38Z"
+}
+
+JSON;
     }
 }
